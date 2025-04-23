300a19a035ebeaa6b2604a0fa1f064c53c98861d
Thimaz000 <delllap152@gmail.com>
2025-04-23 22:10:26 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'April 2, 2025',
+  birthdate: 'April 24, 2025',
   name: ' Bosa...!!'
 };
 
