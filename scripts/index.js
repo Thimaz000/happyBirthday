43829ce7bc41e30d3182d6fd68a34925f611e56d
Thimaz000 <delllap152@gmail.com>
2025-04-23 21:56:17 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -50,7 +50,7 @@ x = setInterval(function() {
     opts = {
       strings: ['HAPPY', 'BIRTHDAY!', config.name ,'Wish you all the best to future..','Jaya wewa...!!!'],
       charSize: 20,
-      charSpacing: 30,
+      charSpacing: 10,
       lineHeight: 40,
 
       cx: w / 2,
